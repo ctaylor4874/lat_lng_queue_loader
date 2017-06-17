4b44d7276e6adfe3340d599356ef20ee7499db15
Cody Taylor <ctaylor4874@gmail.com>
2017-06-17 12:56:54 -0500

    Added error logic for lat/lng.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import os
 import logging
 from json import dumps
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 import sqs
 
 app = Flask(__name__)
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+SECRET = os.getenv('QUEUE_LOADER_SECRET').encode()
 
 
 def send_message(queue, data):
@@ -18,6 +19,7 @@ def get_data(result):
     start_lng = float(result.get('start_lng'))
     end_lat = float(result.get('end_lat'))
     end_lng = float(result.get('end_lng'))
+
     data = {
         'start_lat': start_lat,
         'start_lng': start_lng,
@@ -38,7 +40,12 @@ def handle_data():
     if request.method == 'POST':
         result = request.form
         data = get_data(result)
-        send_message(lat_lng_queue, dumps(data))
+        if data.get('start_lat') >= data.get('end_lat'):
+            flash("Starting latitude must me less than ending latitude.", category='error')
+        elif data.get('start_lng') >= data.get('end_lng'):
+            flash("Starting longitude must me less than ending longitude.", category='error')
+        else:
+            send_message(lat_lng_queue, dumps(data))
         return redirect(url_for('index'))
     return render_template('error.html')
 
@@ -46,6 +53,7 @@ def handle_data():
 if __name__ == '__main__':
     logging.basicConfig(level=20, format='%(asctime)s:{}'.format(logging.BASIC_FORMAT))
     try:
+        app.config['SECRET_KEY'] = bytes(SECRET)
         app.run(debug=False)
     except Exception as e:
         logging.exception(e)
